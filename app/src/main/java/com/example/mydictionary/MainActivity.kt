8a4a6966aa75a9108ff404ecd495d7ecca2e6055
rogoz208 <rogoz208@gmail.com>
2022-04-08 17:29:57 +0300

    lesson-1: enable viewBinding in MainActivity.kt

--- app/src/main/java/com/example/mydictionary/MainActivity.kt
+++ app/src/main/java/com/example/mydictionary/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mydictionary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.mydictionary.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }

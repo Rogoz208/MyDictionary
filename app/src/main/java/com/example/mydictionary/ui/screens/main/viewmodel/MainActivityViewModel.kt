744dcb98b46fd5281a002e5f0f5aebef1294bcc3
rogoz208 <rogoz208@gmail.com>
2022-04-25 20:36:36 +0300

    lesson-5: used CoroutineExceptionHandler instead of try catch

--- app/src/main/java/com/example/mydictionary/ui/screens/main/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/mydictionary/ui/screens/main/viewmodel/MainActivityViewModel.kt
@@ -4,24 +4,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.mydictionary.domain.entities.WordEntity
 import com.example.mydictionary.domain.repos.Repository
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.IOException
 
 class MainActivityViewModel(private val repo: Repository) :
     MainActivityViewModelContract.ViewModel() {
 
-    override val wordsLiveData: MutableLiveData<List<WordEntity>> = MutableLiveData<List<WordEntity>>()
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        errorLiveData.postValue(throwable.message)
+    }
+
+    override val wordsLiveData: MutableLiveData<List<WordEntity>> =
+        MutableLiveData<List<WordEntity>>()
     override val errorLiveData: MutableLiveData<String> = MutableLiveData<String>()
 
     override fun getData(word: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val words = repo.getData(word)
-                wordsLiveData.postValue(words)
-            } catch (e: IOException) {
-                errorLiveData.postValue(e.localizedMessage)
-            }
+        viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler) {
+            val words = repo.getData(word)
+            wordsLiveData.postValue(words)
         }
     }
 }
